ab78e82d627ffa3f77a7cee1baa97b39c525d714
ZHANG Meng <ztz2000@gmail.com>
2022-05-20 22:31:35 +0800

    Auto-Completion
    - remove fixed preview window during completion
    - auto-completion
      - add `nvim-cmp`, `cmp-nvim-lsp`, `vim-vsnip`
      - revise `lspconfig` setup

--- lua/plugins/nvim_lspconfig.lua
+++ lua/plugins/nvim_lspconfig.lua
@@ -1,4 +1,13 @@
--- Mappings.
+-- ============================================================================
+-- reference
+-- 1. https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion
+-- 2. https://github.com/hrsh7th/nvim-cmp
+-- ============================================================================
+
+-- completeopt
+vim.cmd [[set completeopt=menu,menuone,noselect]]
+
+-- Mappings
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
 local keymap = function(lhs, rhs)
@@ -10,15 +19,12 @@ keymap('[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
 keymap(']d', '<cmd>lua vim.diagnostic.goto_next()<CR>')
 keymap('<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>')
 
--- Use an on_attach function to only map the following keys
+-- use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(_, bufnr)
     local buf_keymap = function(lhs, rhs)
         return vim.api.nvim_buf_set_keymap(bufnr, 'n', lhs, rhs, opts)
     end
-
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
     -- Mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
@@ -39,30 +45,40 @@ local on_attach = function(_, bufnr)
         '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>')
 end
 
--- require 'lspconfig'.jedi_language_server.setup {
+local lspconfig = require('lspconfig')
+-- add additional capabilities supported by nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+-- lspconfig.jedi_language_server.setup {
 --     on_attach = on_attach,
+--     capabilities = capabilities,
 --     cmd = { "jedi-language-server" },
 -- }
 
-require 'lspconfig'.pyright.setup {
+lspconfig.pyright.setup {
     on_attach = on_attach,
+    capabilities = capabilities,
 }
 
-require 'lspconfig'.clangd.setup {
+lspconfig.clangd.setup {
     on_attach = on_attach,
+    capabilities = capabilities,
     cmd = { "/opt/homebrew/opt/llvm/bin/clangd" },
 }
 
-require 'lspconfig'.rust_analyzer.setup {
+lspconfig.rust_analyzer.setup {
     on_attach = on_attach,
+    capabilities = capabilities,
 }
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-require 'lspconfig'.sumneko_lua.setup {
+lspconfig.sumneko_lua.setup {
     on_attach = on_attach,
+    capabilities = capabilities,
     settings = {
         Lua = {
             runtime = {
@@ -87,4 +103,39 @@ require 'lspconfig'.sumneko_lua.setup {
             },
         },
     },
+}
+
+local cmp = require 'cmp'
+cmp.setup {
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` user
+            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` user
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` user
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` user
+        end,
+    },
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-o>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' }, -- For vsnip users.
+        -- { name = 'luasnip' }, -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
+    }, {
+        { name = 'buffer' },
+    })
 }
