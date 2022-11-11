1d30b8f3ed184ba1aced8bbe66547ac25369f35c
ppmzhang2 <50707839+ppmzhang2@users.noreply.github.com>
2022-11-12 11:02:20 +1300

    Support Fish and Fixes (#45)
    - support fish syntax
    - load with `bash` as `fish` is too slow
    - ignore .luarc.json
    - modify outdated

--- lua/plugins/nvim_treesitter.lua
+++ lua/plugins/nvim_treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "bash", "c", "cpp", "lua", "markdown", "python",
+    ensure_installed = { "fish", "bash", "c", "cpp", "lua", "markdown", "python",
         "rst", "rust", },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
